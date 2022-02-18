5e88f97d09ee7ad657bb66da5ae98728b0945301
Sankhadip Pal <saffron450@gmail.com>
2022-02-18 21:09:51 +0530

    rest all

---
+++ src/com/Rusty/Main.java
@@ -0,0 +1,15 @@
+package com.Rusty;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int num = 1; num < n; num++) {
+            System.out.println(num + "");
+
+        }
+
+    }
+}
